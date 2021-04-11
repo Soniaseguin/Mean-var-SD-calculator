2f3394958614ee5e96e4bc5b74c51b4899972714
Sonia Seguin <>
2021-04-11 20:00:27 +0000

    Final solution of the problem

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,30 +1,20 @@
 import numpy as np
-list=[1,2,3,4,5,6,7,8,9]
+
 def calculate(list):
-  arr = np.reshape(list,(3,3))
-  calculations ={
-    'mean' : [np.mean(arr, axis=1), np.mean(arr,axis=0), np.mean(arr)], 
-    'variance' : [np.var(arr,axis=1), np.var(arr,axis=0), np.var(arr)],
-    'standard deviation': [np.std(arr,axis=1), np.std(arr,axis=0), np.std(arr)], 
-    'max': [np.max(arr, axis=1), np.max(arr,axis=0), np.max(arr)], 
-    'min': [np.min(arr, axis=1), np.min(arr, axis=0), np.min(arr)],
-    'sum': [np.sum(arr,axis=1), np.sum(arr,axis=0), np.sum(arr)] 
-  }
-  print(calculations)
+  if len(list) == 9:
+    arr = np.reshape(list,(3,3))
+    calculations ={
+      'mean' : [np.mean(arr,axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(arr).tolist()], 
+      'variance' : [np.var(arr,axis=0).tolist(), np.var(arr,axis=1).tolist(), np.var(arr).tolist()],
+      'standard deviation': [np.std(arr,axis=0).tolist(), np.std(arr,axis=1).tolist(), np.std(arr).tolist()], 
+      'max': [np.max(arr, axis=0).tolist(), np.max(arr,axis=1).tolist(), np.max(arr).tolist()], 
+      'min': [np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(arr).tolist()],
+      'sum': [np.sum(arr,axis=0).tolist(), np.sum(arr,axis=1).tolist(), np.sum(arr).tolist()] 
+    }
+  else:
+    raise ValueError("List must contain nine numbers.")
 
   return calculations
 
-""""
-1 convert the list into a 3 x 3 Numpy array,
-2 return a dictionary containing the mean, variance, standard deviation, max, min, and sum along both axes and for the flattened matrix.
-{
-  'mean': [axis1, axis2, flattened],
-  'variance': [axis1, axis2, flattened],
-  'standard deviation': [axis1, axis2, flattened],
-  'max': [axis1, axis2, flattened],
-  'min': [axis1, axis2, flattened],
-  'sum': [axis1, axis2, flattened]
 
- 4 - CHECK RAISE ERROR
-  If a list containing less than 9 elements is passed into the function, it should raise a ValueError exception with the message: "List must contain nine numbers." The values in the returned dictionary should be lists and not Numpy arrays.
-}"""
+
